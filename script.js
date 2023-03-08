9459a989e2b7ccd7521586f2a7dc5bda88ccdb37
Nicolas <nicolas10morschel@gmail.com>
2023-03-08 16:13:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+class Ator{
+    constructor(nome, nacionalidade){
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+    }
+}
+
+class Diretor{
+    constructor(nome, nacionalidade){
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+    }
+}
+class Filme{
+    constructor(id, titulo, ano, genero, duracao, sinopse, cartaz, direcao, elenco, classificacao, avaliacao){
+        this.id = id;
+        this.titulo = titulo;
+        this.ano = ano;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.cartaz = cartaz;
+        this.direcao = direcao;
+        this.elenco = elenco;
+        this. classificacao = classificacao;
+        this.avaliacao = avaliacao;
+    }
+}
